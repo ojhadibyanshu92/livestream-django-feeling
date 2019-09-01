53538b047147031a1a4fa689c4eae1450d9a85b7
dibyanshu <ojhadibyanshu92@gmail.com>
2019-09-01 15:46:43 +0530

    logout view ,form and template

--- feeling/users/urls.py
+++ feeling/users/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns =[
     url(r'^dashboard/$',views.dashboard,name='dashboard'),
+    url(r'^logout/$',views.LogoutView.as_view(),name='logout'),
     url('^',include('django.contrib.auth.urls'))
 ]
--- feeling/users/views.py
+++ feeling/users/views.py
@@ -1,4 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
+from django.core.urlresolvers import reverse
+from django.views.generic import FormView
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from . import forms
+
 def dashboard(request):
     return render(request,'users/dashboard.html')
+
+class LogoutView(LoginRequiredMixin,FormView):
+    form_class = forms.LogoutForm
+    template_name='users/logout.html'
+    def form_valid(self,form):
+        logout(self.request)
+        return HttpResponseRedirect(reverse('home'))
