c52fb375e8fb5088ab620daec9539c110832146d
dibyanshu <ojhadibyanshu92@gmail.com>
2019-09-01 19:43:50 +0530

    singup template view and etc

--- feeling/users/views.py
+++ feeling/users/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import logout
 from django.core.urlresolvers import reverse
-from django.views.generic import FormView
+from django.views.generic import FormView,CreateView
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
+from django.core.urlresolvers import reverse,reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . import forms
 
@@ -16,3 +17,9 @@ class LogoutView(LoginRequiredMixin,FormView):
     def form_valid(self,form):
         logout(self.request)
         return HttpResponseRedirect(reverse('home'))
+
+class SignupView(CreateView):
+    form_class = UserCreationForm
+    template_name = 'users/signup.html'
+    success_url = reverse_lazy('users:dashboard')
+
